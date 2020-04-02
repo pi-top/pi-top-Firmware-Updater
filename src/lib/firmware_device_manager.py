672e839d70468306a43275c628141120c12159f0
Jorge Capona <jcapona@gmail.com>
2020-04-02 10:00:15 -0300

    Add type hinting, udpate variable names

--- src/lib/firmware_device_manager.py
+++ src/lib/firmware_device_manager.py
@@ -20,45 +20,45 @@ class DeviceInfoKeys(Enum):
 
 
 class FirmwareDeviceManager:
-    __device_info = {}
+    __devices_status = {}
     file_monitor = None
     queue = None
     FIRMWARE_FILE_PATH = '/usr/lib/pt-firmware-updater/bin/'
 
-    def __init__(self, devices):
-        self.devices = devices
+    def __init__(self, devices: [FirmwareDeviceID]) -> None:
+        self.devices_id_list = devices
         for dev in devices:
-            self.__device_info[dev] = {}
+            self.__devices_status[dev] = {}
         self.scan()
 
-    def scan(self, packet_interval=None):
+    def scan(self, packet_interval: float = None):
         PTLogger.debug('Scanning for connected firmware devices')
 
-        for dev in self.devices:
+        for dev in self.devices_id_list:
             try:
                 fw_device = FirmwareDevice(dev, packet_interval)
                 connected = True
                 PTLogger.debug('{} is connected'.format(dev))
-                self.__device_info[dev][DeviceInfoKeys.FW_DEVICE] = fw_device
+                self.__devices_status[dev][DeviceInfoKeys.FW_DEVICE] = fw_device
             except (ConnectionError, AttributeError, PTInvalidFirmwareDeviceException) as e:
                 PTLogger.warning('{} - {}'.format(dev.name, e))
                 connected = False
-                self.__device_info[dev] = {}
+                self.__devices_status[dev] = {}
             except Exception as e:
                 PTLogger.error('{} - {}'.format(dev.name, e))
                 connected = False
-                self.__device_info[dev] = {}
+                self.__devices_status[dev] = {}
             finally:
-                self.__device_info[dev][DeviceInfoKeys.CONNECTED] = connected
+                self.__devices_status[dev][DeviceInfoKeys.CONNECTED] = connected
 
-    def connected_devices(self):
-        return [device_id for device_id in self.__device_info if self.is_connected(device_id)]
+    def connected_devices(self) -> [FirmwareDeviceID]:
+        return [device_id for device_id in self.__devices_status if self.is_connected(device_id)]
 
-    def is_connected(self, device: FirmwareDeviceID) -> bool:
-        return self.__device_info[device][DeviceInfoKeys.CONNECTED] if device in self.__device_info else False
+    def is_connected(self, device_id: FirmwareDeviceID) -> bool:
+        return self.__devices_status[device_id][DeviceInfoKeys.CONNECTED] if device_id in self.__devices_status else False
 
-    def force_update_if_available(self):
-        for device_id in self.devices:
+    def force_update_if_available(self) -> None:
+        for device_id in self.devices_id_list:
             if self.has_update(device_id):
                 self.update(device_id)
             self.set_notification_status(device_id, True)
@@ -69,7 +69,7 @@ class FirmwareDeviceManager:
             return has_updates
 
         try:
-            fw_dev = self.__device_info[device_id][DeviceInfoKeys.FW_DEVICE]
+            fw_dev = self.__devices_status[device_id][DeviceInfoKeys.FW_DEVICE]
             fw_updater = FirmwareUpdater(fw_dev)
             has_updates = fw_updater.update_available()
         except (ConnectionError, AttributeError, PTInvalidFirmwareDeviceException) as e:
@@ -82,8 +82,8 @@ class FirmwareDeviceManager:
             fw_updater = None
         finally:
             self.set_notification_status(device_id, has_updates)
-            self.__device_info[device_id][DeviceInfoKeys.UPDATE_AVAILABLE] = has_updates
-            self.__device_info[device_id][DeviceInfoKeys.FW_UPDATER] = fw_updater
+            self.__devices_status[device_id][DeviceInfoKeys.UPDATE_AVAILABLE] = has_updates
+            self.__devices_status[device_id][DeviceInfoKeys.FW_UPDATER] = fw_updater
         return has_updates
 
     def update(self, device_id: FirmwareDeviceID) -> bool:
@@ -91,12 +91,12 @@ class FirmwareDeviceManager:
         if not self.is_connected(device_id):
             PTLogger.info("{} is not connected".format(device_id))
             return success
-        if not self.__device_info[device_id][DeviceInfoKeys.UPDATE_AVAILABLE]:
+        if not self.__devices_status[device_id][DeviceInfoKeys.UPDATE_AVAILABLE]:
             PTLogger.info("{} - There's no update available".format(device_id))
             return success
 
         try:
-            fw_updater = self.__device_info[device_id][DeviceInfoKeys.FW_UPDATER]
+            fw_updater = self.__devices_status[device_id][DeviceInfoKeys.FW_UPDATER]
             PTLogger.info("{} - Updating firmware".format(device_id))
 
             if fw_updater.install_updates():
@@ -110,16 +110,16 @@ class FirmwareDeviceManager:
 
         return success
 
-    def set_notification_status(self, device, status):
-        self.__device_info[device][DeviceInfoKeys.NOTIFIED] = status
+    def set_notification_status(self, device_id: FirmwareDeviceID, status: bool) -> None:
+        self.__devices_status[device_id][DeviceInfoKeys.NOTIFIED] = status
 
-    def was_notified(self, device):
-        return self.__device_info[device][DeviceInfoKeys.NOTIFIED]
+    def was_notified(self, device_id: FirmwareDeviceID):
+        return self.__devices_status[device_id][DeviceInfoKeys.NOTIFIED]
 
-    def start_file_supervisor(self):
+    def start_file_supervisor(self) -> None:
         self.queue = multiprocessing.Queue()
         event_manager = FirmwareFileEventManager(self.queue)
-        binary_directories = [self.FIRMWARE_FILE_PATH + dev.name for dev in self.devices]
+        binary_directories = [self.FIRMWARE_FILE_PATH + dev.name for dev in self.devices_id_list]
 
         self.file_monitor = FileSupervisor(binary_directories, event_manager)
         self.file_monitor.run(threaded=True)
