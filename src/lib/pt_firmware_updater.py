85ed306075dbd128d1c02db98cd1d0a467abf5b6
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-05-04 13:54:08 +0100

    Use `i2cping` (#28)

--- src/lib/pt_firmware_updater.py
+++ src/lib/pt_firmware_updater.py
@@ -16,7 +16,7 @@ from core.notification_manager import NotificationManager, UpdateStatusEnum
 
 def i2c_addr_found(device_address: int) -> bool:
     try:
-        run_command(f"pt-i2cdetect {device_address}", timeout=1, check=True, log_errors=False)
+        run_command(f"i2cping {device_address}", timeout=1, check=True, log_errors=False)
         is_connected = True
     except Exception:
         is_connected = False
